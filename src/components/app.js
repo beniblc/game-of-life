5ea2e43e978910cb8217ef6f22a94f4fa24cbe15
Ben Crisostomo <beni.blc@gmail.com>
2017-10-05 17:14:08 -0400

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state={grid: this.makeGrid(5,10)}
+  }
+
+  makeGrid = (row, col) => {
+    var grid =[];
+    for (var i=1;i<=row;i++){
+      var rowVal = []
+      for(var k=1;k<=col;k++){
+        rowVal.push({col:k, live: false})
+      }
+      grid.push(rowVal)
+    };
+    return grid;
+  }
+
   render() {
+    console.log(this.state.grid)
     return (
-      <div>React simple starter</div>
-    );
+      <div>
+        <svg width="200" height="200">
+          <rect width="5" height="5" />React simple starter
+        </svg>
+      </div>
+    )
   }
 }
