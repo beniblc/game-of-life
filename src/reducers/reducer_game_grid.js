02c801d7d1eb0b15d50868b1e37c5f956522a94e
Ben Crisostomo <beni.blc@gmail.com>
2017-11-09 13:24:21 -0500

    last working version before big change

--- src/reducers/reducer_game_grid.js
+++ src/reducers/reducer_game_grid.js
@@ -1,24 +1,18 @@
 import  { ACTIVE_BOARD } from '../actions';
 
-export default function(action) {
+export default function() {
 
-    switch(state={}, action.type){
-        case ACTIVE_BOARD:
-            return action.payload;
-
-        default: 
-            //format of grid - {col: x, live: false}
-            const makeGrid = (row, col) => {
-                var grid =[];
-                for (var i=0;i<row;i++){
-                    var rowVal = []
-                    for(var k=0;k<col;k++){
-                        rowVal.push({id: (col*i+(k+1)), live: false})
-                    }
-                    grid.push(rowVal)
-                }
-                return grid;
+    //format of grid - {col: x, live: false}
+    const makeGrid = (row, col) => {
+        var grid =[];
+        for (var i=0;i<row;i++){
+            var rowVal = []
+            for(var k=0;k<col;k++){
+                rowVal.push({id: (col*i+(k+1)), live: false})
             }
-            return makeGrid(50, 90); 
-    } 
+            grid.push(rowVal)
+        }
+        return grid;
+    }
+    return makeGrid(50, 90); 
 }
